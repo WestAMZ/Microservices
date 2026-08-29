104cea270e45367fae708b21a15932d091ff4d05
Westly Meza <westlymeza@gmail.com>
2026-08-28 23:28:24 -0600

    Adding filter component to choose pageSize

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,24 +5,27 @@ import { AppPagination } from "../components/AppPagination";
 import { useEffect, useState } from "react";
 import { Auction } from "@/types";
 import { getData } from "../actions/auctionActions";
+import { Filters } from "./Filters";
 
 export  const Listings = () => {
     
     const [auctions, setAuction] = useState<Auction[]>([]);
     const [pageCount, setPageCount] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(4);
 
     useEffect(() => {
-        getData(pageNumber).then(data =>{
+        getData(pageNumber, pageSize).then(data =>{
             setAuction(data.results);
             setPageCount(data.pageCount);
         })
-    }, [pageNumber]);
+    }, [pageNumber, pageSize]);
 
     if(auctions.length ===0) return <h3>Loading...</h3>
 
     return (
         <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize}/>
             <div className="grid grid-cols-4 gap-6">
                 {auctions && auctions.map((auction) => (
                 <AuctionCard key={auction.id} auction={auction }/>
