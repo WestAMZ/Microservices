c23b1838c87dab1528deb2b06bd965899d78399c
Westly Meza <westlymeza@gmail.com>
2026-08-29 15:27:28 -0600

    Adding search input to navbar

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineCar  } from "react-icons/ai"
+import { Search } from "./Search"
 export default function NavBar() {
   return (
     <header className="stiky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
@@ -6,7 +7,7 @@ export default function NavBar() {
             <AiOutlineCar size={34}/>
             <div>Carsties Auctions</div>
         </div>
-        <div>Search</div>
+        <Search/>
         <div>Login</div>
     </header>
   )
---
+++ frontend/web-app/app/nav/Search.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useParamsStore } from "@/hooks/userParamsStore"
+import { ChangeEvent, useState } from "react";
+import { FaSearch } from "react-icons/fa"
+
+export const Search = () => {
+    const setParams = useParamsStore(state => state.setParams);
+    const [value, setValue] = useState('');
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>){
+        setValue(e.target.value);
+    }
+
+    function handleSearch(){
+        setParams({searchTerm:value});
+    }
+
+    return (
+        <div className="flex w-[50%] items-center border-2 border-gray-300 rounded-full py-2 shadow-sm">
+            <input
+                onKeyDown={(e)=>{
+                    if(e.key === 'Enter'){
+                        handleSearch();
+                    }
+                }}
+                onChange={handleChange}
+                value={value}
+                type="text"
+                placeholder="Search fro cars by made, model or color"
+                className="
+                    grow
+                    pl-5
+                    bg-transparent
+                    focus:outline-none
+                    border-transparent
+                    focus:border-transparent
+                    focus:ring-0
+                    text-sm
+                    text-gray-600
+                "
+            />
+            <button onClick={handleSearch}> 
+                <FaSearch size={34} 
+                    className="bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"
+                />
+            </button>
+        </div>
+    )
+}
