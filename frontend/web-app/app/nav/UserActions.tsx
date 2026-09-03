f76b39c21f023f85fcb5c473fccddefbcde927ed
Westly Meza <westlymeza@gmail.com>
2026-09-03 15:55:31 -0600

    Adding user actions to navbar

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,10 +1,37 @@
-import { Button } from "flowbite-react"
-import Link from "next/link"
+'use client'
 
-export const UserActions = () => {
+import { Dropdown, DropdownDivider, DropdownItem } from "flowbite-react"
+import { User } from "next-auth"
+import { signOut } from "next-auth/react"
+import Link from "next/link"
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai"
+import { HiCog, HiUser } from "react-icons/hi2"
+
+type Props = {
+  user: User
+}
+
+export const UserActions = ({ user }: Props) => {
   return (
-    <Button>
-        <Link href="/session">Session</Link>
-    </Button>
+    <Dropdown inline label={`Welcome ${user.username}`} className="cursor-pointer">
+        <DropdownItem icon={HiUser}>
+          My Auctions
+        </DropdownItem>
+        <DropdownItem icon={AiFillTrophy}>
+          Auctions won
+        </DropdownItem>
+        <DropdownItem icon={AiFillCar}>
+          Sell my car
+        </DropdownItem>
+        <DropdownItem icon={HiCog}>
+          <Link href="/session">
+          Session (dev only!)
+          </Link>
+        </DropdownItem>
+        <DropdownDivider />
+        <DropdownItem icon={AiOutlineLogout} onClick={() => signOut({ redirectTo: '/' })}>
+          Signout
+        </DropdownItem>
+    </Dropdown>
   )
 }
