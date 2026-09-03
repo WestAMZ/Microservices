7d7cc08081bc3a7144ecee0d56ea76c51e874867
Westly Meza <westlymeza@gmail.com>
2026-09-03 13:09:07 -0600

    Populationg user information in session

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -1,7 +1,8 @@
 import NextAuth, { Profile } from "next-auth"
 import { OIDCConfig } from "next-auth/providers"
 import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6"
- 
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     // Configure the Duende Identity Server provider (matching Config.cs file in IdentityService)
@@ -12,6 +13,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         issuer: "http://localhost:5000", // The URL of your Duende Identity Server
         authorization: { params: { scope: 'openid profile auctionApp' } },
         idToken: true
-    } as OIDCConfig<Profile>),
+    } as OIDCConfig<Omit<Profile, 'username'>>),
   ],
+  // Callbacks allow you to control what happens when a user signs in, signs out, or when a session is created or updated. 
+  // You can use these callbacks to customize the behavior of your authentication flow.
+  callbacks:{
+    async jwt({ token, profile }) {
+      
+      if(profile){
+        token.username = profile.username;
+      }
+      return token;
+    },
+    
+    async session({ session, token}){
+      if(token){
+        session.user.username = token.username;
+      }
+      return session;
+    }
+  }
 })
